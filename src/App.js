8db0be0162e188baf40cedcafa5e9a07c857dee6
Chris Williams <cwill833@gmail.com>
2019-07-29 15:23:33 -0500

    file structure done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import getCurrentLatLng from './utils/Geolocator'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+        myLat: null,
+        myLng: null,
+        weWorkLat: 30.2685,
+        weWorkLng: -97.743
+    }
+  }
+
+  componentDidMount(){
+    getCurrentLatLng().then(results =>{
+      this.setState({
+        myLat: results.lat,
+        myLng: results.lng
+      })
+    })
+  }
+
+  render(){
+    let latDif = Math.abs(this.state.myLat - this.state.weWorkLat).toFixed(4)
+    let lngDif = Math.abs(this.state.myLng - this.state.weWorkLng).toFixed(3)
+
+    let clockInLat = !parseInt(latDif) ? "Clocked in": "Not clocked in"
+    let clockInLng = !parseInt(lngDif) ? "Clocked in": "Not clocked in"
+    
+    return(
+      <div className="flex-r">
+        <h1>Hello World!</h1>
+        <h2>This is my location</h2>
+        <h3>lat: {this.state.myLat}</h3>
+        <h3>lng: {this.state.myLng}</h3>
+        <h4>This is the differece in Lat: {clockInLat}</h4>
+        <h4>This is the differece in Lat: {clockInLng}</h4>
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/utils/Geolocator.js
@@ -0,0 +1,11 @@
+function getCurrentLatLng() {
+    // Wrap getCurrentPosition to return a promise
+    return new Promise(resolve => {
+      navigator.geolocation.getCurrentPosition(pos => resolve({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude
+      }));
+    });
+  }
+
+export default getCurrentLatLng;
